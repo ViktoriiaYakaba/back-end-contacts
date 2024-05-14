dbf7f1665a77b432c99b0b1558d6987667d3b964
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-14 16:19:48 +0200

    summary

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const { contactsRouter } = require("./routes/contactsRouter");
-const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
 
-dotenv.config()
 
 app.use(morgan("tiny"));
 app.use(cors());
---
+++ models/contacts.js
@@ -0,0 +1,26 @@
+const { required } = require('joi');
+const { Schema, model } = require('mongoose');
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    }
+}, { versionKey: false, timestamps: true });
+
+const Contact = model("contact", contactSchema);
+
+module.exports = Contact;
+ 
