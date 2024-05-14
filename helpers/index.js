ac17eca3a255c3588f299f80d0e225f76fd10ece
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-14 17:41:19 +0200

    update

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,14 @@
 const HttpError = require("./HttpError");
 const ErrorHandler = require("./ErrorHandler");
 const validateBody = require("./validateBody");
+const handlerMongooseError = require("./HandlerMongooseError");
+const isValidId= require('./isValidId')
 
 module.exports = {
     HttpError,
     ErrorHandler,
     validateBody,
+    handlerMongooseError,
+    isValidId,
 }
 
--- models/contacts.js
+++ models/contacts.js
@@ -1,5 +1,6 @@
-const { required } = require('joi');
 const { Schema, model } = require('mongoose');
+const { handlerMongooseError } = require('../helpers');
+const Joi = require("joi");
 
 const contactSchema = new Schema({
     name: {
@@ -20,7 +21,29 @@ const contactSchema = new Schema({
     }
 }, { versionKey: false, timestamps: true });
 
+contactSchema.post("save", handlerMongooseError);
+
+const addSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+    favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+    favorite: Joi.boolean().required(),
+});
+
+
+const shemas = {
+    addSchema,
+    updateFavoriteSchema,
+};
+
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+module.exports = {
+    Contact,
+    shemas,
+};
  
