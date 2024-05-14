3fb757182976ff605ec6bc4af8044ae5fd8f6cf9
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-14 17:47:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST} = process.env;
 
 mongoose.set('strictQuery', true)
 
 
 mongoose.connect(DB_HOST)
-    .then(() => app.listen( PORT))
+    .then(() => app.listen( 3000))
     .catch(error => {
         console.log(error.message);
         process.exit(1)
